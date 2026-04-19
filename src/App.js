7e03e51bb16b78e59b4e158c048b07255736e927
ProgramadorVictor <victorochaandrade@gmail.com>
2026-04-19 10:59:26 -0300

    Spread Operator, map, cadastrando tarefas

--- src/App.js
+++ src/App.js
@@ -1,59 +1,40 @@
 import { useState } from "react";
 
 function App(){
-  const [nome, setNome] = useState();
-  const [idade, setIdade] = useState();
-  const [email, setEmail] = useState();
-  const [user, setUser] = useState({});
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz'
+  ]);
+
   
   function handleRegister(e){
-    e.preventDefault(); //Prevenindo o comportamento padrão do form
+    e.preventDefault();
+    
+    setTarefas([...tarefas, input]) //Spread Operador 
+    setInput('')
 
-    alert('Usuario registrado com sucesso');
-
-    setUser({ //Trabalhando com um objeto, associando as variaveis ao objeto.
-      nome: nome,
-      idade: idade,
-      email: email
-    });
   }
 
   return (
     <div>
-      <h1>Cadastrando Usuario</h1>
+      <h1>Cadastrando Tarefas</h1>
       <form onSubmit={handleRegister}>
         <label>Nome:</label><br />
         <input
           type="text"
-          placeholder="Digite seu nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
+          placeholder="Nome da Tarefa"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         /><br />
 
-        <label>E-mail:</label><br />
-        <input
-        type="email"
-        placeholder="Digite seu e-mail"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        /><br />
-
-        <label>Idade:</label><br />
-        <input
-        type="number"
-        placeholder="Digite seu idade"
-        value={idade}
-        onChange={(e) => setIdade(e.target.value)}
-        /><br />
-
-        <button>Enviar</button>
+        <button>Cadastrar</button>
       </form>
 
-      <div>
-        <span>Bem vindo: {user.nome}</span><br />
-        <span>Idade: {user.idade}</span><br />
-        <span>Email: {user.email}</span><br />
-      </div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li> //Toda lista deve ter um id/key para o react não se perder.
+        ))}
+      </ul>
     </div>
   );
 }
