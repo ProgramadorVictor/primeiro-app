d68048fe68bf224c5b7617f9ecb649bd00b77c2f
ProgramadorVictor <victorochaandrade@gmail.com>
2026-04-22 10:53:07 -0300

    useState e localStorage cadastrando tarefas

--- src/App.js
+++ src/App.js
@@ -1,42 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App(){
-  const [input, setInput] = useState('');
-  const [tarefas, setTarefas] = useState([
-    'Pagar a conta de luz'
-  ]);
+    const [input, setInput] = useState('');
+    const [tarefas, setTarefas] = useState([]);
 
-  
-  function handleRegister(e){
-    e.preventDefault();
+    useEffect(() => {
+        const tarefasStorage = localStorage.getItem('@tarefas');
+        if(tarefasStorage){
+            setTarefas(JSON.parse(tarefasStorage));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('@tarefas', JSON.stringify(tarefas));
+    }, [tarefas]); //Assistindo uma propriedade, reage a mudança
+
     
-    setTarefas([...tarefas, input]) //Spread Operador 
-    setInput('')
+    function handleRegister(e){
+        e.preventDefault();
+        
+        setTarefas([...tarefas, input]) //Spread Operador 
+        setInput('')
 
-  }
+    }
 
-  return (
-    <div>
-      <h1>Cadastrando Tarefas</h1>
-      <form onSubmit={handleRegister}>
-        <label>Nome:</label><br />
-        <input
-          type="text"
-          placeholder="Nome da Tarefa"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        /><br />
+    return (
+        <div>
+        <h1>Cadastrando Tarefas</h1>
+        <form onSubmit={handleRegister}>
+            <label>Nome:</label><br />
+            <input
+            type="text"
+            placeholder="Nome da Tarefa"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            /><br />
 
-        <button>Cadastrar</button>
-      </form>
+            <button>Cadastrar</button>
+        </form>
 
-      <ul>
-        {tarefas.map(tarefa => (
-          <li key={tarefa}>{tarefa}</li> //Toda lista deve ter um id/key para o react não se perder.
-        ))}
-      </ul>
-    </div>
-  );
+        <ul>
+            {tarefas.map(tarefa => (
+            <li key={tarefa}>{tarefa}</li> //Toda lista deve ter um id/key para o react não se perder.
+            ))}
+        </ul>
+        </div>
+    );
 }
 
 export default App;
